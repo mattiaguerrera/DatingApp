3a7d2e66e87e6141f8ebe50e373e7cfe7ae2d62c
Mattia Guerrera <latuaemail@dominio.com>
2019-03-26 09:12:10 +0100

    End of Section 8

---
+++ DatingApp.API/Controllers/UsersControllers.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _repository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IDatingRepository repository, IMapper mapper)
+        {
+            this._mapper = mapper;
+            this._repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _repository.GetUsers();
+            
+            var usersToReturn = _mapper.Map<IEnumerable<UserForDetailDto>>(users);
+
+            return Ok(usersToReturn);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _repository.GetUser(id);
+
+            var userToReturn = _mapper.Map<UserForDetailDto>(user);
+
+            return Ok(userToReturn);
+        }
+
+    }
+}
