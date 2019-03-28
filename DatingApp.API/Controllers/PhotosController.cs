e7d2bb9c1119ca34d4b009a0c3253f7fc4c63dc5
Mattia Guerrera <latuaemail@dominio.com>
2019-03-28 13:11:49 +0100

    End-1 of Section 11

---
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -0,0 +1,102 @@
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Helpers;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DatingApp.API.Controllers
+{
+    [Authorize]
+    [Route("api/users/{userId}/photos")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private readonly IDatingRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+
+        private Cloudinary _cloudinary;
+
+        public PhotosController(IDatingRepository repository,         
+                                IMapper mapper,        
+                                IOptions<CloudinarySettings> cloudinaryConfig) 
+        {
+            this._repository = repository;
+            this._mapper = mapper;
+            this._cloudinaryConfig = cloudinaryConfig;
+
+            Account account = new Account(
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+            
+            _cloudinary = new Cloudinary(account);
+        }
+
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public async Task<IActionResult> GetPhoto(int id) 
+        {
+            var photoFromRepo = await _repository.GetPhoto(id);
+            var photo = _mapper.Map<PhotoForCreationDto>(photoFromRepo);
+
+            return Ok(photo);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoUser(int userId, 
+                                                      [FromForm]PhotoForCreationDto photoForCreationDto) 
+        {            
+            // controllo tramite il token se l'id passato corrisponde all'utente con cui siamo loggati             
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repository.GetUser(userId);
+
+            var file = photoForCreationDto.File;
+
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0) 
+            {
+                using (var stream = file.OpenReadStream()) 
+                {
+                    var uploadParams = new ImageUploadParams() {
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadParams);
+                }
+            }
+
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);
+            if (!userFromRepo.Photos.Any(u => u.IsMain))
+                photo.IsMain = true;
+
+            userFromRepo.Photos.Add(photo);
+            
+            if (await _repository.SaveAll()) 
+            {
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+                return CreatedAtRoute("GetPhoto", new { id = photo.Id}, photoToReturn);
+            }
+
+            return BadRequest("Could not add the photo");
+        }
+    }
+}
